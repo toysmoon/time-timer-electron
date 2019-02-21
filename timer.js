aa4c0acbc93dc0369d8b688c24a6d121b4ed5e7d
2016-061 Jo Sung Jung <16061sungjung@ecount.kr>
2019-02-21 18:30:12 +0900

    [DEV] 화면 구성 및 기능 개발 1. layout 수정 2. node notifier 를 사용 시스템 알림

--- timer.js
+++ timer.js
@@ -1,14 +1,11 @@
-// html references
-
-let optionsButton = document.querySelector('#optionsButton');
-let addClockButton = document.querySelector('#addClockButton');
-let silenceButton = document.querySelector('#silenceButton');
+const notifier = require('node-notifier');
 
 // globals
 
 let maxFps = 60;
 let maxMinutes = 60;
 let newRemainingMinutes = 0;
+let endTime = null;
 
 // functions
 
@@ -28,7 +25,7 @@ function startTimer(timer) {
   if (!remainingMinutes) {
     return;
   };
-  timer.setAttribute('targetTime', remainingMinutes * 60 * 1000 + Date.now());
+  endTime = new Date(remainingMinutes * 60 * 1000 + Date.now());
   timer.removeAttribute('remainingMinutes');
 
   let timerID = timer.getAttribute('timerID');
@@ -38,14 +35,13 @@ function startTimer(timer) {
       ((timer) => {
         return () => {
           if (!newRemainingMinutes) {
-            let targetTime = timer.getAttribute('targetTime');
             let remainingMinutes;
-            if (targetTime < Date.now()) {
-              alert('Done!');
+            if (endTime < Date.now()) {
+              notifier.notify({title: "Time Timer", message: "Times over"});
               stopTimer(timer);
               remainingMinutes = 0;
             } else {
-              remainingMinutes = (targetTime - Date.now()) / 60 / 1000;
+              remainingMinutes = (endTime - Date.now()) / 60 / 1000;
             }
             drawTime(timer, remainingMinutes, maxMinutes);
           }
@@ -112,55 +108,34 @@ function drawTime(timer, remainingMinutes /*Double*/, maxMinutes /* Integer */) 
     ? ("0" + seconds).slice(-2) + '"'
     : "";
   
-  fillTextCenter(ctx, canvas, first);
-  fillTextDown(ctx, canvas, second);
+  fillTextUp(ctx, canvas, getEndTimeText(endTime));
+  fillTextCenter(ctx, canvas, first + ' ' + second);
+  //fillTextDown(ctx, canvas, second);
 }
 
-fillTextCenter = (ctx, canvas, text) => {
-  ctx.fillStyle = 'black';
-  ctx.textAlign = "center";
-  ctx.font = "40px Arial";
-  ctx.textBaseline = "middle";
-  ctx.fillText(text, canvas.width / 2 + 5, canvas.height / 2 - 5);
+const getEndTimeText = (endTime) => {
+  const h = ("0" + endTime.getHours()).slice(-2);
+  const m = ("0" + endTime.getMinutes()).slice(-2);
+  return h + ':' + m;  
 }
 
-fillTextDown = (ctx, canvas, text) => {
-  ctx.fillStyle = 'grey';
+const fillTextUp = (ctx, canvas, text) => {
+  ctx.fillStyle = 'red';
   ctx.textAlign = "center";
   ctx.font = "15px Arial";
-  ctx.textBaseline = "top";
-  ctx.fillText(text, canvas.width / 2 + 2, canvas.height / 2 + 20);
+  ctx.textBaseline = "middle";
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2 - 33);
 }
 
-function drawTimerdigits(timer, maxMinutes) { // will be called once, when a new timer is created or resized
-  maxMinutes = maxMinutes || 60;
-  let canvas = timer.querySelector('.analogTimer');
-  let ctx = canvas.getContext("2d");
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = 'white';
-  ctx.textBaseline = "middle";
+const fillTextCenter = (ctx, canvas, text) => {
+  ctx.fillStyle = 'black';
   ctx.textAlign = "center";
-  ctx.font = "30px Arial";
-  ctx.fillText(0, canvas.width / 2, canvas.height / 2);
-
-  ctx.textAlign = "center";
+  ctx.font = "25px Arial";
   ctx.textBaseline = "middle";
-  for (let i = 0; i < 12; i++) {
-    if (!(i % 3)) {
-      ctx.font = "20px Arial";
-      ctx.fillStyle = 'white';
-    } else {
-      ctx.font = "15px Arial";
-      ctx.fillStyle = 'grey';
-    }
-    ctx.fillText(Math.round(maxMinutes * (1 - i / 12) * 100) / 100, canvas.width / 2 * (1 + 0.9 * Math.cos((i / 6 - 0.5) * Math.PI)), canvas.height / 2 * (1 + 0.9 * Math.sin((i / 6 - 0.5) * Math.PI)));
-  }
+  ctx.fillText(text, canvas.width / 2 + 3, canvas.height / 2);
 }
 
 // events
-
 document.addEventListener('click', (event) => {
   if (event.target.className == 'analogTimer' && newRemainingMinutes > 0) {
     setStartTime(event.target.parentElement, newRemainingMinutes);
@@ -172,7 +147,7 @@ document.addEventListener('click', (event) => {
 document.addEventListener('mousemove', (event) => {
   if (event.target.className == 'analogTimer' && event.buttons == 1) {
     newRemainingMinutes = getMinutesFromPosition(event.offsetX, event.offsetY, event.target.width, event.target.height, maxMinutes);
-    // this.debug.innerText = 'new start time: ' + newRemainingMinutes;
+    endTime = new Date(newRemainingMinutes * 60 * 1000 + Date.now());
     drawTime(event.target.parentElement, newRemainingMinutes, maxMinutes);
   }
 }, false);
